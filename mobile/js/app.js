f03bae5b9c9169425e935750c890c42a57ef99ff
Nick Deakin <nickdeakin83@gmail.com>
2019-09-09 12:06:50 +0100

    Added methods for isFirstRun, setAppHasRun, getAppVersion and getAppName

--- mobile/js/app.js
+++ mobile/js/app.js
@@ -1,14 +1,85 @@
-function closeApp() {
-  var appManager = new Appworks.AWAppManager(
+document.addEventListener("deviceready", onDeviceReady, false);
+var mAppManager = null;
+
+function onDeviceReady() {
+  mAppManager = new Appworks.AWAppManager(
     function(result){
       out(result);
     },
     function(error){
       out(error);
     });
-
-  appManager.closeActiveApp();
 }
+
+function closeApp() {
+  mAppManager.closeActiveApp();
+}
+
+function appName() {
+  appManager = new Appworks.AWAppManager(
+    function(result){
+      out('App Name: ' + result);
+    },
+    function(error){
+      out(error);
+    });
+
+    mAppManager.getAppName();
+}
+
+function appVersion() {
+  mAppManager.getAppVersion(
+    // Success function
+    function(version) {
+        out("Version: " + version);
+    },
+    // Error function
+    function(err) {
+        out("Error getting app version: " + err);
+    });
+}
+
+function isFirstRun() {
+  mAppManager.isFirstRun(
+    // Success function
+    function(isFirstRun) {
+        out("Is First Run?... " + isFirstRun);
+    },
+    // Error function
+    function(err) {
+        out("Error getting isFirstRun: " + err);
+    });
+}
+
+function setAppHasRun() {
+  mAppManager.setAppHasRun(
+    // Success function
+    function() {
+        out("Has has been mark as ran, isFirstRun will now be false");
+    },
+    // Error function
+    function(err) {
+        out("Error getting isFirstRun: " + err);
+    });
+}
+
+function shouldClearCache() {
+  // Identical to isFirstRun()
+  mAppManager.shouldClearCache(
+    // Success function
+    function(isFirstRun) {
+        out("Should Clear Cache?... " + isFirstRun);
+    });
+}
+
+function resetShouldClearCache() {
+  // Identical to setAppHasRun();
+  mAppManager.shouldClearCache();
+}
+
+/********************
+** Utility methods **
+********************/
 
 function out(message) {
   console.log(message);
